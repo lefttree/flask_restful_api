012b1f9c948d56f55c21158e72559a3fb73fa92f
Xiang <xiang@silvustechnologies.com>
2015-11-25 12:31:52 -0800

    Feat: make json format 404 response

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!env/bin/python
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -35,6 +35,11 @@ def get_task(task_id):
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(debug=True)
