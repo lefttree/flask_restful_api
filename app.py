3114fbb9b20276bef2e6a82c3cd673d5596fb2bd
Xiang <xiang@silvustechnologies.com>
2015-11-25 12:30:09 -0800

    add another GET api

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!env/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -28,6 +28,13 @@ def index():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 if __name__ == "__main__":
     app.run(debug=True)
